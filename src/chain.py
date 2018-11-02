4ff2b923cc9c185ac3fb9f97785633e57c4a085e
Simon Wahlström <simon.otdw@gmail.com>
2018-11-02 22:46:15 +0100

    feat: blacklisting corpus files

--- src/chain.py
+++ src/chain.py
@@ -9,6 +9,7 @@ class Chain():
     """Markov Chain Generator"""
     def __init__(self, corpus_path, model=None):
         self.corpus_path = corpus_path
+        self.blacklist = "blacklist.txt"
         self.corpus_pairs = None
         self.corpus = None    # Splitted words
         self.model = None
@@ -18,9 +19,16 @@ class Chain():
         """Build a new Model from Corpus"""
         # Load corpus
         txt = ""
+        blacklist = list() # Ignore blacklisted corpus files
+        if os.path.isfile(self.blacklist):
+            with open(self.blacklist, encoding='utf8') as f:
+                blacklist = f.read().split("\n")
+
         if os.path.isdir(self.corpus_path):
             for (dirpath, _, filenames) in os.walk(self.corpus_path):
                 for filename in filenames:
+                    if filename in blacklist:
+                        continue
                     with open(os.path.join(dirpath, filename)) as f:
                         txt += f.read()
         else:
@@ -54,6 +62,8 @@ class Chain():
         self.values = [first_word]
 
         for _ in range(steps):
+            # TODO: If there are too few words possible, maybe pick another
+            # random word.
             self.values.append(self.walk(self.model[self.values[-1]]))
 
         self.values[0] = self.values[0].title()
@@ -88,7 +98,7 @@ class Chain():
     def walk(self, chain):
         """
         Pick the next step at random
-        Each key appearance count is turned into a probability.
+        Each key appearance rate is turned into a probability.
         Then a random key is selected at random, based on probability.
         """
         keys = list(chain.keys())
